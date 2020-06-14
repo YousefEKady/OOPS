89f2555f4459524029d4515460e50174f80bfb96
Yousef ElKady <43918525+YousefEKady@users.noreply.github.com>
2020-06-14 14:23:49 +0200

    Add files via upload

---
+++ assets/js/plugin.js
@@ -0,0 +1,15 @@
+﻿// Search Picker
+$(document).ready(function () {
+    $('.search-pick').selectpicker();
+});
+// Product Picker
+$(document).ready(function () {
+    $('.product-pick').selectpicker();
+});
+// Car Ad Form
+$(document).ready(function () {
+    var value = $(".forValue").val();
+    if (value == 1) {
+        $(".for-hidden-car").show();
+    }
+});
